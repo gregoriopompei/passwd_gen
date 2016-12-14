0f5355e6ca759b7f6c77d3ec6fd4a78a9d1ce7d4
Gregorio Pompei <pompeigregorio@gmail.com>
2016-12-14 21:53:17 +0100

    Update random_password.rb

--- random_password.rb
+++ random_password.rb
@@ -1,9 +1,3 @@
-#print "What is the website for the password? (i.e.:www.example.com) "
-#website = gets.chomp 
-#print "And what is the username for the account? (i.e.:username) "
-#username = gets.chomp
-#print "Finally what is your email? (i.e.:your.email@example.com) "
-#email = gets.chomp
 print "Insert your password length: "
 password = gets
   o = [('a'..'z'), ('A'..'Z'), (0 .. 9)].map { |i| i.to_a }.flatten
@@ -13,11 +7,6 @@ password = gets
 
   (0...password.to_i).map {('a'..'z').to_a[rand(15)] }.join
 
-#File.open("credential.txt", 'w') do |scrivi|
-#scrivi.puts "The website is #{website}\n The username is #{username}\n The email is #{email}\n And the password is #{string}\n"
-#end
-
-  
   print "This is your password : #{string}\n"
   print "And this is the length of it: #{string.length}\n"
   
